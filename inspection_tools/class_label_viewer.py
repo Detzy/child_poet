3c73e83f35ab8908dabdd8510a7943fc3401ab31
Eirik Ølberg <eirik.olberg@gmail.com>
2022-05-30 12:09:34 +0200

    generate_histogram_of_specific_dataset added

--- inspection_tools/class_label_viewer.py
+++ inspection_tools/class_label_viewer.py
@@ -1,6 +1,7 @@
 import os
 import collections
 import matplotlib.pyplot as plt
+import pandas as pd
 import obstacle_detector.data_preprocessing as dp
 
 
@@ -12,6 +13,54 @@ except ImportError:
 
 """
 """
+
+
+def generate_histogram_of_specific_dataset(csv_file_path, output_path):
+    """
+    This function plots a histogram for a specific data set.
+    Parameters
+    ----------
+    csv_file_path   :   str
+                        Path to the csv file containing the data.
+    output_path     :   str
+                        Path to the output folder.
+
+    Returns
+    -------
+    None
+    """
+    # Load the data
+    data = pd.read_csv(csv_file_path)
+    data = data['cluster']
+
+    # Plot the histogram
+    ymin = 0.5
+    ymax = 3000
+
+    plt.rcParams["figure.figsize"] = [7.00, 3.50]
+    plt.rcParams["figure.autolayout"] = True
+
+    cluster_sizes = collections.Counter(data)
+
+    highest_bin = max(list(cluster_sizes.values())) + 1
+    steps = 20
+
+    plt.title(label="Cluster distribution after pre-processing")
+    plt.ylabel(ylabel="Number of clusters")
+    plt.xlabel(xlabel="Cluster size")
+
+    plt.hist(
+        cluster_sizes.values(),
+        bins=steps,
+        range=(0, highest_bin),
+        log=True
+    )
+
+    ax = plt.gca()
+    ax.set_ylim([ymin, ymax])
+
+    plt.savefig(output_path + "/post_processing_histrogram.png")
+    plt.close()
 
 
 def save_to_file(figure, cluster_label, output_folder_path):
@@ -38,7 +87,7 @@ def plot_clustering_samples(clustering, img_filenames, threshold, class_samples,
 
             img = plt.imread(img_filename)
 
-            fig.add_subplot(3, 4, count + 1)
+            fig.add_subplot(1, 2, count + 1)
             plt.imshow(img)
             count += 1
             if count == class_samples:
@@ -55,14 +104,20 @@ def main(img_folder, label_data_path, output_path, threshold, class_samples):
 
 if __name__ == '__main__':
     threshold = 30
-    class_samples = 10
+    class_samples = 2
 
     k = '30'
     lr = '0_1'
     img_folder = r'/uio/hume/student-u31/eirikolb/img/poet_dec2_168h/img_files'
     label_data = r'/uio/hume/student-u31/eirikolb/img/img_clusters/img_k30_lr0_1_threshold30_imbalance_degree4.csv'
-    out_data_dir = "/uio/hume/student-u31/eirikolb/tmp/class_comparison_k{}_lr{}".format(k, lr)
+    out_data_dir = "/uio/hume/student-u31/eirikolb/tmp/class_comparison_k{}_lr{}_v2".format(k, lr)
 
     main(img_folder, label_data, out_data_dir, threshold, class_samples)
+
+    # data_path = \
+    #     r'/uio/hume/student-u31/eirikolb/img/img_clusters/img_k30_lr0_1_threshold30_imbalance_degree4_man_relabel.csv'
+    # output_path = \
+    #     r'/uio/hume/student-u31/eirikolb/Documents/child_poet/inspection_tools/_plots'
+    # generate_histogram_of_specific_dataset(data_path, output_path)
 
 
